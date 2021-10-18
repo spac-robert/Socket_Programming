9a0c7a34d2390c1a6ce220400b8a2161b601be4f
spac-robert <spac-robert@github.com>
2021-10-18 23:18:17 +0300

    solving ex 2 using iterative serve - client

--- Homework_1_a/src/ro/robert/ex_2/Client_ex_2.java
+++ Homework_1_a/src/ro/robert/ex_2/Client_ex_2.java
@@ -1,4 +1,28 @@
 package ro.robert.ex_2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client_ex_2 {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 6666);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+            String string = scanner.readLine();
+            writer.println(string);
+            writer.flush();
+
+            int number = Integer.parseInt(reader.readLine());
+            System.out.println(number);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
--- Homework_1_a/src/ro/robert/ex_2/Server_ex_2.java
+++ Homework_1_a/src/ro/robert/ex_2/Server_ex_2.java
@@ -1,16 +1,39 @@
 package ro.robert.ex_2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server_ex_2 {
+
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(6666);
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected!");
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+                String string = reader.readLine();
+                writer.println(numberOfSpaces(string));
+                writer.flush();
+            }
+
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    private static int numberOfSpaces(String string) {
+        int number = 0;
+        for (int i = 0; i < string.length() && string.charAt(i) != '\0'; i++) {
+            if (string.charAt(i) == ' ') {
+                number++;
+            }
+        }
+        return number;
     }
 }
